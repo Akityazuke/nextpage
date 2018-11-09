e868d3ccd28d8b93b8630d8610a656954e97b780
DESKTOP-IO03Q79\あきちゃずけ <c01161966c@edu.teu.ac.jp>
2018-11-09 11:14:43 +0900

    add function Ajax for to obtain recipe data

--- top/top_search.js
+++ top/top_search.js
@@ -1,7 +1,32 @@
 // HTMLが全て読み込まれたら実行
+// 検索カテゴリー・検索ワードをPHPに送信
 $().ready(function(){
-    $('#s_top').click(function(){
+    $('form').submit(function(){
         var s_cate = $('option:selected').val();
         var s_word = $('#s_word').val();
+        var $search　= {'category':s_cate,'word':s_word};
+        console.log(s_cate);
+        console.log(s_word);
+
+//[非同期通信]index.htmlの検索窓で選択された内容送信
+        $.ajax('http://localhost:8091/nextpage/php/top_main.php',{
+            type: "POST",
+            data: $search,
+            datatype: 'json',
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                //未実装->Ajax通信ができなかった時の処理
+            },
+            success: function(response){
+                //PHPからの値を受け取る
+                if(s_cate == 'all' || s_cate == 'liqu'){
+                    //未実装->search_result.htmlにレシピデータ受け渡し
+                }else {
+                    //未実装->search_result_recipe.htmlにレシピデータ受け渡し
+                }
+            }
+        });
     });
 });
+
+// 現在の課題
+// - indexhtmlでカテゴリーを選択後、どのようにレシピの選別を行うか
