d4555ae20d691825f95dea5257ac7947a98baa70
DESKTOP-IO03Q79\あきちゃずけ <c01161966c@edu.teu.ac.jp>
2018-11-14 08:43:41 +0900

    add description for connection to DB

--- php/top_main.php
+++ php/top_main.php
@@ -1,4 +1,33 @@
 <?php
-    echo $_POST['category'];
-    echo $_POST['word'];
+print_r(get_magic_quotes_gpc());
+
+//検索で選択した検索項目とワードからレシピ（またはお酒）をDBから取得する
+    //POSTデータ
+    $postData = file_get_contents('php://input');
+    $search_json = json_decode($postData,true); 
+
+    //DBのユーザネームとパスワード
+    $url = "../pass_json/pass.json";
+    $json = file_get_contents($url);
+    $pass_json = json_decode($json,true);
+
+    $pdo = new PDO('mysql:host=localhost;dbname=nextpage;port=3306;charset=utf8',$pass_json['db_user'],$pass_json['db_pass']);
+
+    //検索ワードから取得するデータを選択
+    if($search_json['word'] == '%%'){
+        $recipe = sprintf("SELECT * FROM recipedb");
+        $stmt = $pdo -> query($recipe);
+    }else if($search_json['word'] != ''){
+        $search_word = $search_json['word'];
+        $recipe = $pdo -> prepare(
+            "SELECT * FROM recipedb 
+            WHERE content 
+            LIKE :word");
+        $exec_recipe = array(":word"=>$search_word);
+        $recipe -> execute($exec_recipe);
+        $stmt = $recipe;
+    }
+
+    $recipe_table = $stmt->fetchAll();
+    echo json_encode($recipe_table,JSON_UNESCAPED_UNICODE);
 ?>
