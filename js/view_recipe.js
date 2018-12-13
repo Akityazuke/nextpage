91367b35ea107f4bea80e2dd10b9f8511224f411
DESKTOP-IO03Q79\あきちゃずけ <c01161966c@edu.teu.ac.jp>
2018-12-13 11:39:06 +0900

    add description to reference json

--- js/view_recipe.js
+++ js/view_recipe.js
@@ -2,7 +2,7 @@ $().ready(function(){
     var taste = ['甘口','ほんのり甘口','ほんのり辛口','辛口'];
     var density = ['薄味','やや薄味','やや濃味','濃味'];
 
-    $.getJSON('pass_json/recipe_data.json',function(data){
+    $.getJSON('../pass_json/recipe_data.json',function(data){
         $.each(data,function(index,recipe){
             if(index != 0){
                 var input_text = '<table border = "1"><tr><th>'+recipe.name+":"+taste[recipe.taste]+"で"+density[recipe.density]+'</th><tr></table>';
